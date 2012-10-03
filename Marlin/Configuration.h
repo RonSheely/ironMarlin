796b7d2fc248f7f33ccc44a0d11b114fdca8ce9d
Ron Sheely <ron.sheely@ironwoodlabs.com>
2012-10-03 13:34:02 -0700

    scaled and limited z-axis

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -206,7 +206,8 @@ const bool Z_ENDSTOPS_INVERTING = true; // set to true to invert the logic of th
 // #define Y_MAX_POS 205
 #define Y_MAX_POS 140 // 140 mm max Chaucer - ronsheely
 #define Y_MIN_POS 0
-#define Z_MAX_POS 200
+// #define Z_MAX_POS 200
+#define Z_MAX_POS 110 // 110 mm max Chaucer - ronsheely
 #define Z_MIN_POS 0
 
 #define X_MAX_LENGTH (X_MAX_POS - X_MIN_POS)
@@ -229,7 +230,7 @@ const bool Z_ENDSTOPS_INVERTING = true; // set to true to invert the logic of th
 // default settings 
 
 // #define DEFAULT_AXIS_STEPS_PER_UNIT   {78.7402,78.7402,200*8/3,760*1.1}  // default steps per unit for ultimaker 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {66.6666,66.6666,200*8/3,760*1.1}  // default steps per unit for Chaucer - ronsheely 
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {66.6666,66.6666,2560.0,760*1.1}  // default steps per unit for Chaucer - ronsheely 
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 5, 45}    // (mm/sec)    
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
 
