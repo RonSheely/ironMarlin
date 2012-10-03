bdb914c28441f88f175435485e16342f51712622
Ron Sheely <ron.sheely@ironwoodlabs.com>
2012-10-03 11:37:14 -0700

    set max x and y software stops

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -200,9 +200,11 @@ const bool Z_ENDSTOPS_INVERTING = true; // set to true to invert the logic of th
 #define min_software_endstops true //If true, axis won't move to coordinates less than HOME_POS.
 #define max_software_endstops true  //If true, axis won't move to coordinates greater than the defined lengths below.
 // Travel limits after homing
-#define X_MAX_POS 205
+// #define X_MAX_POS 205
+#define X_MAX_POS 160 // 160 mm max for Chaucer - ronsheely
 #define X_MIN_POS 0
-#define Y_MAX_POS 205
+// #define Y_MAX_POS 205
+#define Y_MAX_POS 140 // 140 mm max Chaucer - ronsheely
 #define Y_MIN_POS 0
 #define Z_MAX_POS 200
 #define Z_MIN_POS 0
@@ -227,7 +229,7 @@ const bool Z_ENDSTOPS_INVERTING = true; // set to true to invert the logic of th
 // default settings 
 
 // #define DEFAULT_AXIS_STEPS_PER_UNIT   {78.7402,78.7402,200*8/3,760*1.1}  // default steps per unit for ultimaker 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {66.6666,66.6666,200*8/3,760*1.1}  // default steps per unit for pocket printer - ronsheely 
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {66.6666,66.6666,200*8/3,760*1.1}  // default steps per unit for Chaucer - ronsheely 
 #define DEFAULT_MAX_FEEDRATE          {500, 500, 5, 45}    // (mm/sec)    
 #define DEFAULT_MAX_ACCELERATION      {9000,9000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
 
